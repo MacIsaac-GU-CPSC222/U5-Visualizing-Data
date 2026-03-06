8c27753dabd712626485fa69602f36003aba0fed
Dominic MacIsaac <dmacisaac@ewu.edu>
2026-03-06 09:11:32 -0800

    main added

---
+++ VisualizationFun/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+# DATA VIS NOTES
+# EDA: exploratory data analysis
+# getting familiar with your data
+# exploring data, visualizing data, mining data
+
+# goals of data visualization
+# 1. clearly and accurately represent data
+# 2. be creative, with the goal of increasing understanding
+# 3. label units and points of interest
+
+# some jargon
+# chart: a 2D visualization
+# plot: a chart of data points (e.g., scatter plot)
+# graph: a chart of a math function (e.g., sine)
+
+# we will use the matplotlib library for charting
+# 3 ways to use matplotlib
+# 1. using the pyplot submodule
+import matplotlib.pyplot as plt
+
+# there is always a "current" figure
+# https://matplotlib.org/stable/api/pyplot_summary.html
+# 2. using the OOP interface
+# 3. a mix of the two
+
+
+def main():
+    pass
+
+main()
